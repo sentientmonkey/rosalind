f373176bb20b03b9412499d4f78b7fa882bec663
Scott Windsor <swindsor@gmail.com>
2016-02-02 22:58:29 -0800

    Adding DNA count

---
+++ racket/dna.rkt
@@ -0,0 +1,26 @@
+#lang racket/base
+
+(require rackunit)
+
+(define (dna-count c lst)
+  (cond
+    [(null? lst) 0]
+    [(equal? (car lst) c) (add1 (dna-count c (cdr lst)))]
+    [else (dna-count c (cdr lst))]))
+
+(define (dna s)
+  (letrec [(lst (string->list s))
+           (a (dna-count #\A lst))
+           (c (dna-count #\C lst))
+           (g (dna-count #\G lst))
+           (t (dna-count #\T lst))]
+    `(,a ,c ,g ,t)))
+
+(check-equal?
+  (dna "AAAA") '(4 0 0 0))
+
+(check-equal?
+  (dna "ACCG") '(1 2 1 0))
+
+(check-equal?
+  (dna "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC") '(20 12 17 21))
