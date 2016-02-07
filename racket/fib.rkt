5a578e1d951facf48287d501c6384501699b5767
Scott Windsor <swindsor@gmail.com>
2016-02-06 20:00:00 -0800

    Adding dynamic programming approach for fib as well.

--- racket/fib.rkt
+++ racket/fib.rkt
@@ -2,7 +2,18 @@
 
 (require rackunit)
 
-(provide fib)
+(provide fib mfib dfib)
+
+(define (fib n k)
+  (if (< n 3)
+    1
+    (+ (fib (- n 1) k)
+       (* k (fib (- n 2) k)))))
+
+(check-equal? (fib 1 1) 1)
+(check-equal? (fib 2 1) 1)
+(check-equal? (fib 12 1) 144)
+(check-equal? (fib 5 3) 19)
 
 (define-syntax define-memoized
   (syntax-rules ()
@@ -13,13 +24,25 @@
            (hash-ref! results (list args ...)
                       (lambda () bodies ...)))))]))
 
-(define-memoized (fib n k)
+(define-memoized (mfib n k)
   (if (< n 3)
     1
-    (+ (fib (- n 1) k)
-       (* k (fib (- n 2) k)))))
+    (+ (mfib (- n 1) k)
+       (* k (mfib (- n 2) k)))))
 
-(check-equal? (fib 0 1) 1)
-(check-equal? (fib 1 1) 1)
-(check-equal? (fib 12 1) 144)
-(check-equal? (fib 5 3) 19)
+(check-equal? (mfib 1 1) 1)
+(check-equal? (mfib 2 1) 1)
+(check-equal? (mfib 12 1) 144)
+(check-equal? (mfib 5 3) 19)
+
+(define (dfib n k)
+  (let ([v (make-vector n 1)])
+    (for ([i (in-range 2 n)])
+      (vector-set! v i (+ (vector-ref v (- i 1))
+                          (* k (vector-ref v (- i 2))))))
+    (vector-ref v (sub1 n))))
+
+(check-equal? (dfib 1 1) 1)
+(check-equal? (dfib 2 1) 1)
+(check-equal? (dfib 12 1) 144)
+(check-equal? (dfib 5 3) 19)
